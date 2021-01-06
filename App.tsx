47cd553a48733c5a80f3e8bf31a0844fd99d9de9
Devin Jameson <devinjameson@me.com>
2021-01-06 11:59:48 -0500

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,103 @@
+import React, {useState, useRef} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Pressable,
+} from 'react-native';
+
+const CODE_DIGITS = 4;
+
+const CodeInput = () => {
+  const [code, setCode] = useState('');
+  const [containerIsFocused, setContainerIsFocused] = useState(false);
+
+  const codeDigitsArray = new Array(CODE_DIGITS).fill(0);
+
+  const ref = useRef<TextInput>(null);
+
+  const handleOnPress = () => {
+    setContainerIsFocused(true);
+    ref?.current?.focus();
+  };
+
+  const handleOnBlur = () => {
+    setContainerIsFocused(false);
+  };
+
+  const toDigitInput = (_value: number, idx: number) => {
+    const emptyInputChar = ' ';
+    const digit = code[idx] || emptyInputChar;
+
+    const isCurrentDigit = idx === code.length;
+    const isLastDigit = idx === CODE_DIGITS - 1;
+    const isCodeFull = code.length === CODE_DIGITS;
+
+    const isFocused = isCurrentDigit || (isLastDigit && isCodeFull);
+
+    const containerStyle =
+      containerIsFocused && isFocused
+        ? {...style.inputContainer, ...style.inputContainerFocused}
+        : style.inputContainer;
+
+    return (
+      <View key={idx} style={containerStyle}>
+        <Text style={style.inputText}>{digit}</Text>
+      </View>
+    );
+  };
+
+  return (
+    <SafeAreaView style={style.container}>
+      <Pressable style={style.inputsContainer} onPress={handleOnPress}>
+        {codeDigitsArray.map(toDigitInput)}
+      </Pressable>
+      <TextInput
+        ref={ref}
+        value={code}
+        onChangeText={setCode}
+        onSubmitEditing={handleOnBlur}
+        keyboardType="number-pad"
+        returnKeyType="done"
+        maxLength={4}
+        style={style.shadowCodeInput}
+      />
+    </SafeAreaView>
+  );
+};
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputsContainer: {
+    width: '60%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  inputContainer: {
+    borderColor: '#cccccc',
+    borderWidth: 2,
+    borderRadius: 4,
+    padding: 12,
+  },
+  inputContainerFocused: {
+    borderColor: '#0f5181',
+  },
+  inputText: {
+    fontSize: 24,
+    fontFamily: 'Menlo-Regular',
+  },
+  shadowCodeInput: {
+    position: 'absolute',
+    height: 0,
+    width: 0,
+    opacity: 0,
+  },
+});
+
+export default CodeInput;
